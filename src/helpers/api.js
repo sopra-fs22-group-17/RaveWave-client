8d6fa7a73c5c3b78db42c5a64562e084f32f6ec2
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2020-03-03 12:57:46 +0100

    Adjust error handling to work with new errors thrown from backend

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -7,19 +7,26 @@ export const api = axios.create({
 });
 
 export const handleError = error => {
-  if (error.response) {
-    let info = '';
-    info += `\nrequest to: ${error.response.request.responseURL}`;
-    info += `\nstatus code: ${error.response.status}`;
-    info += `\nstatus text: ${error.statusText}`;
-    info += `\nerror: ${error.response.data.error}`;
-    info += `\nerror message: ${error.response.data.message}`;
+  const response = error.response;
+
+  // catch 4xx and 5xx status codes
+  if (response && !!`${response.status}`.match(/^[4|5]\d{2}$/)) {
+    let info = `\nrequest to: ${response.request.responseURL}`;
+
+    if (response.data.status) {
+      info += `\nstatus code: ${response.data.status}`;
+      info += `\nerror: ${response.data.error}`;
+      info += `\nerror message: ${response.data.message}`;
+    } else {
+      info += `\nstatus code: ${response.status}`;
+      info += `\nerror message:\n${response.data}`;
+    }
 
     console.log('The request was made and answered but was unsuccessful.', error.response);
     return info;
   } else {
     if (error.message.match(/Network Error/)) {
-      alert('The server cannot be reached. Did you start it?');
+      alert('The server cannot be reached.\nDid you start it?');
     }
 
     console.log('Something else happened.', error);
