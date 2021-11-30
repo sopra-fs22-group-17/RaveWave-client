780ca04cc95a45262d968bba3f09836993bed4a6
Chesta <chesta@protonmail.com>
2021-11-30 19:24:20 +0100

    Game: refactor to functional components

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import styled from 'styled-components';
-import { BaseContainer } from '../../helpers/layout';
-import { api, handleError } from '../../helpers/api';
+import {BaseContainer} from '../../helpers/layout';
+import {api, handleError} from '../../helpers/api';
 import Player from '../../views/Player';
-import { Spinner } from '../../views/design/Spinner';
-import { Button } from '../../views/design/Button';
-import { withRouter } from 'react-router-dom';
+import {Spinner} from '../../views/design/Spinner';
+import {Button} from '../../views/design/Button';
+import {useHistory} from 'react-router-dom';
 
 const Container = styled(BaseContainer)`
   color: white;
@@ -24,29 +24,26 @@ const PlayerContainer = styled.li`
   justify-content: center;
 `;
 
-class Game extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      users: null
-    };
-  }
+const Game = () => {
+  const history = useHistory();
+  const [users, setUsers] = useState(null);
 
-  logout() {
+  const logout = () => {
     localStorage.removeItem('token');
-    this.props.history.push('/login');
+    history.push('/login');
   }
 
-  async componentDidMount() {
+  useEffect(async () => {
     try {
       const response = await api.get('/users');
+
       // delays continuous execution of an async operation for 1 second.
       // This is just a fake async call, so that the spinner can be displayed
       // feel free to remove it :)
       await new Promise(resolve => setTimeout(resolve, 1000));
 
       // Get the returned users and update the state.
-      this.setState({ users: response.data });
+      setUsers(response.data);
 
       // This is just some data for you to see what is available.
       // Feel free to remove it.
@@ -58,41 +55,41 @@ class Game extends React.Component {
       // See here to get more data.
       console.log(response);
     } catch (error) {
-      alert(`Something went wrong while fetching the users: \n${handleError(error)}`);
+      console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
+      console.error("Details:", error);
+      alert("Something went wrong while fetching the users! See the console for details.");
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <Container>
-        <h2>Happy Coding! </h2>
-        <p>Get all users from secure end point:</p>
-        {!this.state.users ? (
-          <Spinner />
-        ) : (
-          <div>
-            <Users>
-              {this.state.users.map(user => {
-                return (
-                  <PlayerContainer key={user.id}>
-                    <Player user={user} />
-                  </PlayerContainer>
-                );
-              })}
-            </Users>
-            <Button
-              width="100%"
-              onClick={() => {
-                this.logout();
-              }}
-            >
-              Logout
-            </Button>
-          </div>
-        )}
-      </Container>
+  let content = <Spinner/>;
+
+  if (users) {
+    content = (
+      <div>
+        <Users>
+          {users.map(user => (
+            <PlayerContainer key={user.id}>
+              <Player user={user}/>
+            </PlayerContainer>
+          ))}
+        </Users>
+        <Button
+          width="100%"
+          onClick={() => logout()}
+        >
+          Logout
+        </Button>
+      </div>
     );
   }
+
+  return (
+    <Container>
+      <h2>Happy Coding! </h2>
+      <p>Get all users from secure end point:</p>
+      {content}
+    </Container>
+  );
 }
 
-export default withRouter(Game);
+export default Game;
