f9c566ccc7228924c42faaed09fa7a1e05daf500
Chesta <chesta@protonmail.com>
2021-12-15 13:34:16 +0100

    Login: create FormField subcomponent

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -5,6 +5,35 @@ import {useHistory} from 'react-router-dom';
 import {Button} from 'components/ui/Button';
 import 'styles/views/Login.scss';
 import BaseContainer from "components/ui/BaseContainer";
+import PropTypes from "prop-types";
+
+/*
+It is possible to add multiple components inside a single file,
+however be sure not to clutter your files with an endless amount!
+As a rule of thumb, use one file per component and only add small,
+specific components that belong to the main one in the same file.
+ */
+const FormField = props => {
+  return (
+    <div className="login field">
+      <label className="login label">
+        {props.label}
+      </label>
+      <input
+        className="login input"
+        placeholder="enter here.."
+        value={props.value}
+        onChange={e => props.onChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+FormField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
 
 const Login = props => {
   const history = useHistory();
@@ -33,21 +62,15 @@ const Login = props => {
     <BaseContainer>
       <div className="login container">
         <div className="login form">
-          <label className="login label">
-            Username
-          </label>
-          <input
-            className="login input"
-            placeholder="enter here.."
-            onChange={e => setUsername(e.target.value)}
+          <FormField
+            label="Username"
+            value={username}
+            onChange={un => setUsername(un)}
           />
-          <label className="login label">
-            Name
-          </label>
-          <input
-            className="login input"
-            placeholder="enter here.."
-            onChange={e => setName(e.target.value)}
+          <FormField
+            label="Name"
+            value={name}
+            onChange={n => setName(n)}
           />
           <div className="login button-container">
             <Button
