0009c058c951ebfac11a44911224b40dfd3fb0a6
Marco Leder <marco.leder@uzh.ch>
2022-04-06 14:19:59 +0200

    document not defined error, dont know how to fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../App";
 
-
 /**
  * This is the entry point of your React application where the root element is in the public/index.html.
  * We call this a “root” DOM node because everything inside it will be managed by React DOM.
  * Applications built with just React usually have a single root DOM node.
  * More: https://reactjs.org/docs/rendering-elements.html
  */
-ReactDOM.render(<App />, document.getElementById("root"));
+
+function Index() {
+        return ReactDOM.render(<App/>, document.getElementById("root"));
+}
+
+
+export default Index;
