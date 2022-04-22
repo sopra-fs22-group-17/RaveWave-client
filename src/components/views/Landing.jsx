17984abe470042aefa4b478ed477f2ea8673847f
Marco Leder <marco.leder@uzh.ch>
2022-04-22 15:35:56 +0200

    landing improvements

--- src/components/views/Landing.jsx
+++ src/components/views/Landing.jsx
@@ -1,16 +1,28 @@
 import {useHistory} from 'react-router-dom';
 import BaseContainer from "components/ui/BaseContainer";
 import { Button } from '@mantine/core';
+import Lottie from 'react-lottie';
+import animationData from './lotties/RaveWaveAnimation.json';
 
 const Landing = (props) => {
     // use react-router-dom's hook to access the history
     const history = useHistory();
 
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+
     return (
         <BaseContainer className="game container">
             <p className="game paragraph">RaveWave Host</p>
-            <Button onClick={() => history.push('/login')} width="100%">login</Button>
-            <Button onClick={() => history.push('/register')} width="100%">register</Button>
+            <Lottie options={defaultOptions} height={200} width={200}/>
+            <Button onClick={() => history.push('/register')} width="100%">Register</Button>
+            <Button onClick={() => history.push('/login')} width="100%">Login</Button>
         </BaseContainer>
     );
 }
