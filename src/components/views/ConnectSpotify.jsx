c8571c1a1172dc139fcd894189e92ec5edd5b6ad
marcoleder.crypto <marco.leder@uzh.ch>
2022-04-25 11:25:19 +0200

    connectspotify & guest are the same, button still different, why?

--- src/components/views/ConnectSpotify.jsx
+++ src/components/views/ConnectSpotify.jsx
@@ -8,12 +8,35 @@ import BaseContainer from "components/ui/BaseContainer";
 
 import "styles/views/ConnectSpotify.scss";
 
+//a
+const FormField = (props) => {
+    return (
+        <div className="login field">
+            <label>{props.label}</label>
+            <input className="login input" placeholder="enter here.." value={props.value} onChange={(e) => props.onChange(e.target.value)} />
+        </div>
+    );
+};
+
+FormField.propTypes = {
+    label: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+};
+//a
+
 const ConnectSpotify = (props) => {
     const history = useHistory();
+    const [username, setUsername] = useState(null); //a
 
     return (
         <BaseContainer className="container">
-            <Button onClick={() => history.push('/')} class="container connectSpotify">Connect Spotify</Button>
+            <div className="container column-item">
+                <FormField label="Username" value={username} onChange={(un) => setUsername(un)} />
+            </div>
+            <Button onClick={() => history.push('/')} class="container connectSpotify">
+                Connect Spotify
+            </Button>
         </BaseContainer>
     );
 };
