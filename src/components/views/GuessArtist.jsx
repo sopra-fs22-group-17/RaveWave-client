51e59b11059fe320ea2986b4a139f7e1833ca0a6
marcoleder.crypto <marco.leder@uzh.ch>
2022-04-26 13:18:37 +0200

    player guessartist refinements

--- src/components/views/GuessArtist.jsx
+++ src/components/views/GuessArtist.jsx
@@ -20,36 +20,6 @@ import "styles/views/GuessArtist.scss";
 
 const buttons = [<Button key="one">One</Button>, <Button key="two">Two</Button>, <Button key="three">Three</Button>, <Button key="four">Four</Button>];
 
-const WallPaper = styled("div")({
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    top: 0,
-    left: 0,
-    overflow: "hidden",
-    background: "linear-gradient(rgb(255, 38, 142) 0%, rgb(255, 105, 79) 100%)",
-    transition: "all 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s",
-    "&:before": {
-        content: '""',
-        width: "140%",
-        height: "140%",
-        position: "absolute",
-        top: "-40%",
-        right: "-50%",
-        background: "radial-gradient(at center center, rgb(62, 79, 249) 0%, rgba(62, 79, 249, 0) 64%)",
-    },
-    "&:after": {
-        content: '""',
-        width: "140%",
-        height: "140%",
-        position: "absolute",
-        bottom: "-50%",
-        left: "-30%",
-        background: "radial-gradient(at center center, rgb(247, 237, 225) 0%, rgba(247, 237, 225, 0) 70%)",
-        transform: "rotate(30deg)",
-    },
-});
-
 const Widget = styled("div")(({ theme }) => ({
     padding: 16,
     borderRadius: 16,
@@ -115,7 +85,7 @@ const GuessArtist = (props) => {
                 <Widget>
                     <Box sx={{ display: "flex", alignItems: "center" }}>
                         <CoverImage>
-                            <img alt="can't win - Chilling Sunday" src="/static/images/sliders/chilling-sunday.jpg" />
+                            <img alt="can't win - Chilling Sunday" src="lotties/albumcover.png" />
                         </CoverImage>
                         <Box sx={{ ml: 1.5, minWidth: 0 }}>
                             <Typography variant="caption" color="text.secondary" fontWeight={500}>
@@ -171,55 +141,7 @@ const GuessArtist = (props) => {
                         <TinyText>{formatDuration(position)}</TinyText>
                         <TinyText>-{formatDuration(duration - position)}</TinyText>
                     </Box>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            mt: -1,
-                        }}
-                    >
-                        <IconButton aria-label="previous song">
-                            <FastRewindRounded fontSize="large" htmlColor={mainIconColor} />
-                        </IconButton>
-                        <IconButton aria-label={paused ? "play" : "pause"} onClick={() => setPaused(!paused)}>
-                            {paused ? (
-                                <PlayArrowRounded sx={{ fontSize: "3rem" }} htmlColor={mainIconColor} />
-                            ) : (
-                                <PauseRounded sx={{ fontSize: "3rem" }} htmlColor={mainIconColor} />
-                            )}
-                        </IconButton>
-                        <IconButton aria-label="next song">
-                            <FastForwardRounded fontSize="large" htmlColor={mainIconColor} />
-                        </IconButton>
-                    </Box>
-                    <Stack spacing={2} direction="row" sx={{ mb: 1, px: 1 }} alignItems="center">
-                        <VolumeDownRounded htmlColor={lightIconColor} />
-                        <Slider
-                            aria-label="Volume"
-                            defaultValue={30}
-                            sx={{
-                                color: theme.palette.mode === "dark" ? "#fff" : "rgba(0,0,0,0.87)",
-                                "& .MuiSlider-track": {
-                                    border: "none",
-                                },
-                                "& .MuiSlider-thumb": {
-                                    width: 24,
-                                    height: 24,
-                                    backgroundColor: "#fff",
-                                    "&:before": {
-                                        boxShadow: "0 4px 8px rgba(0,0,0,0.4)",
-                                    },
-                                    "&:hover, &.Mui-focusVisible, &.Mui-active": {
-                                        boxShadow: "none",
-                                    },
-                                },
-                            }}
-                        />
-                        <VolumeUpRounded htmlColor={lightIconColor} />
-                    </Stack>
                 </Widget>
-                <WallPaper />
             </Box>
         </BaseContainer>
     );
