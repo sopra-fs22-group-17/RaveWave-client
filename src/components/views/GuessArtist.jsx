c413835c24131aaa66b807b41d6a80d82eaf3f25
marcoleder.crypto <marco.leder@uzh.ch>
2022-04-30 15:25:34 +0200

    spotify playback integrated but NOT styled

--- src/components/views/GuessArtist.jsx
+++ src/components/views/GuessArtist.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from "react";
+import React, {useState, useCallback, createRef} from "react";
 import Box from "@mui/material/Box";
 import {Button} from "@mantine/core";
 import Slider from "@mui/material/Slider";
@@ -7,108 +7,38 @@ import Typography from "@mui/material/Typography";
 import BaseContainer from "components/ui/BaseContainer";
 import "styles/views/GuessArtist.scss";
 import {useHistory} from "react-router-dom";
+import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css';
 
-// SPOTIFY STUFF
-import {WebPlaybackSDK, usePlaybackState, useSpotifyPlayer, useWebPlaybackSDKReady, usePlayerDevice} from "react-spotify-web-playback-sdk";
-import SpotifyWebApi from "spotify-web-api-js";
+const songURL = "https://p.scdn.co/mp3-preview/1ac449e52145d1c44dc4346afdb2d8b70e93969b?cid=d7d44473ad6a47cd86c580fcee015449";
 
-let spotifyApi = new SpotifyWebApi();
+const imageURL = "";
 
-spotifyApi.setAccessToken('BQCvV0hdjtrZVI8c8Ezzw4SSoVNAE0wVeVDkoPmnBucsuZ7OsfCOag4hkVOHS1n5WhJhVJAqqMkYBT7kt4IhTQwixirloxtyjxyifW4ifuUsUqQQnLdgBljkmOJd52MkEqbBYjytrYvxm246ZXVGztUbt-DxtgD8W2phDoPQABie8-8wSIJavSg');
-const AUTH_TOKEN = spotifyApi.getAccessToken();
+const SpotifyPlayer = (url, duration) => {
 
-const AppConnectionStateCheck = () => {
-    const webPlaybackSDKReady = useWebPlaybackSDKReady();
-
-    if (!webPlaybackSDKReady) return null;
-
-    return <div>Spotify ready!</div>;
-};
-
-const PlayerDeviceStateCheck = () => {
-    const device = usePlayerDevice();
-
-    if (device === null) return null;
-
-    return <div>Device ready!</div>;
-};
-
-const PlaybackStateCheck = () => {
-    const playbackState = usePlaybackState();
-
-    if (playbackState === null) return null;
-
-    return <p>Connected!</p>;
-};
-
-const PauseResumeButton = () => {
-    const player = useSpotifyPlayer();
-
-    if (player === null) return null;
+    console.log(url);
+    console.log(duration);
 
     return (
-        <div className="guessartist column-item">
-            <button onClick={() => player.pause()}>pause</button>
-            <button onClick={() => player.resume()}>resume</button>
-        </div>
+        <AudioPlayer className="guessartist audioplayer"
+            src={songURL}
+            autoplay={true}
+            showJumpControls={false}
+            showDownloadProgress={false}
+            customProgressBarSection={
+                [
+                    RHAP_UI.CURRENT_TIME,
+                    RHAP_UI.PROGRESS_BAR,
+                    RHAP_UI.DURATION
+                ]
+            }
+            customControlsSection={
+                [
+                    RHAP_UI.MAIN_CONTROLS,
+                ]
+            }
+        />
     );
-};
-
-const MySpotifyPlayer = () => {
-    const getOAuthToken = useCallback(callback => callback(AUTH_TOKEN), []);
-
-    return (
-        <WebPlaybackSDK
-            initialDeviceName="RaveWaveinio"
-            getOAuthToken={getOAuthToken}
-            volume={1.0}
-            connectOnInitialized={true}>
-            {/* `TogglePlay` and `SongTitle` will be defined later. Use player.resume() for init */}
-            <AppConnectionStateCheck/>
-            <PlayerDeviceStateCheck/>
-            <PlaybackStateCheck/>
-            <PauseResumeButton className="guessartist column-item"/>
-        </WebPlaybackSDK>
-    );
-};
-// END SPOTIFY STUFF
-
-const Widget = styled("div")(({theme}) => ({
-    padding: 16,
-    borderRadius: 16,
-    width: 343,
-    maxWidth: "100%",
-    margin: "auto",
-    position: "relative",
-    zIndex: 1,
-    backgroundColor: theme.palette.mode === "dark" ? "rgba(0,0,0,0.6)" : "rgba(255,255,255,0.4)",
-    backdropFilter: "blur(40px)",
-}));
-
-const CoverImage = styled("div")({
-    width: 100,
-    height: 100,
-    objectFit: "cover",
-    overflow: "hidden",
-    flexShrink: 0,
-    borderRadius: 8,
-    backgroundColor: "rgba(0,0,0,0.08)",
-    "& > img": {
-        width: "100%",
-    },
-});
-
-const TinyText = styled(Typography)({
-    fontSize: "0.75rem",
-    opacity: 0.38,
-    fontWeight: 500,
-    letterSpacing: 0.2,
-});
-
-function formatDuration(value) {
-    const minute = Math.floor(value / 60);
-    const secondLeft = value - minute * 60;
-    return `${minute}:${secondLeft < 9 ? `0${secondLeft}` : secondLeft}`;
 }
 
 const GuessArtist = (controller, question) => {
@@ -128,18 +58,6 @@ const GuessArtist = (controller, question) => {
         setAnswered(true);
         controller.answer(question, selection.id);
     };
-
-    async function request() {
-        const myDevices = spotifyApi.getMyDevices();
-
-        console.log(myDevices.data)
-    }
-
-    async function playsample() {
-        const myDevices = spotifyApi.play();
-
-        console.log(myDevices.data)
-    }
 
     return (
         <BaseContainer className="guessartist">
@@ -167,72 +85,13 @@ const GuessArtist = (controller, question) => {
             <Button className="guessartist opt4">
                 Opt4
             </Button>
-            <Widget>
-                <Box sx={{display: "flex", alignItems: "center"}}>
-                    <CoverImage>
-                        <img alt="can't win - Chilling Sunday" src="lotties/chilling-sunday.jpg"/>
-                    </CoverImage>
-                    <Box sx={{ml: 1.5, minWidth: 0}}>
-                        <Typography variant="caption" color="text.secondary" fontWeight={500}>
-                            Jun Pulse
-                        </Typography>
-                        <Typography noWrap>
-                            <b>asd (Can&apos;t win)</b>
-                        </Typography>
-                        <Typography noWrap letterSpacing={-0.25}>
-                            Chilling Sunday &mdash; คนเก่าเขาทำไว้ดี
-                        </Typography>
-                    </Box>
-                </Box>
-                <Slider
-                    aria-label="time-indicator"
-                    size="small"
-                    value={position}
-                    min={0}
-                    step={1}
-                    max={duration}
-                    onChange={(_, value) => setPosition(value)}
-                    sx={{
-                        color: theme.palette.mode === "dark" ? "#fff" : "rgba(0,0,0,0.87)",
-                        height: 4,
-                        "& .MuiSlider-thumb": {
-                            width: 8,
-                            height: 8,
-                            transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
-                            "&:before": {
-                                boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
-                            },
-                            "&:hover, &.Mui-focusVisible": {
-                                boxShadow: `0px 0px 0px 8px ${theme.palette.mode === "dark" ? "rgb(255 255 255 / 16%)" : "rgb(0 0 0 / 16%)"}`,
-                            },
-                            "&.Mui-active": {
-                                width: 20,
-                                height: 20,
-                            },
-                        },
-                        "& .MuiSlider-rail": {
-                            opacity: 0.28,
-                        },
-                    }}
-                />
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        mt: -2,
-                    }}
-                >
-                    <TinyText>{formatDuration(position)}</TinyText>
-                    <TinyText>-{formatDuration(duration - position)}</TinyText>
-                </Box>
-            </Widget>
-            <div className="guessartist column-item">
-                <MySpotifyPlayer />
-            </div>
-            <Button className="guessartist column-item" onClick={request}>
-                RequestStuff
+
+            <SpotifyPlayer url={songURL} duration={20}/>
+
+            <Button className="guessartist opt4">
+                Unmute
             </Button>
+
         </BaseContainer>
     );
 };
