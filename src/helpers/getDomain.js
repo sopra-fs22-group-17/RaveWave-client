dbc139c44af55481f4915efd267d31b51a0f8f18
marcoleder.crypto <marco.leder@uzh.ch>
2022-05-01 22:44:42 +0200

    changed URL for production

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -10,5 +10,5 @@ export const getDomain = () => {
 	const prodUrl = 'https://sopra-fs22-group17-server.herokuapp.com/'; // TODO: insert your groups heroku prod url for server (once deployed)
 	const devUrl = 'http://localhost:8080';
 
-	return devUrl;
+	return prodUrl;
 };
