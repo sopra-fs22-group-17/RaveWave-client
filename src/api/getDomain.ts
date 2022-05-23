df337c416d012ef0ba0c9259d654006d8bfb89ba
v4lentin1879 <valentin@berninabahn.com>
2022-05-23 19:23:48 +0200

    changed produrl

--- src/api/getDomain.ts
+++ src/api/getDomain.ts
@@ -1,8 +1,8 @@
 import {isProduction} from "./isProduction";
 
 export function getDomain(): string {
-    const prodUrl = "http://sopra-fs22-group17-server.herokuapp.com/";
-    const devUrl = "http://localhost:8080/";
+    const prodUrl = "http://localhost:8080";
+    const devUrl = "http://sopra-fs22-group17-server.herokuapp.com/";
 
     return isProduction() ? prodUrl : devUrl;
 }
