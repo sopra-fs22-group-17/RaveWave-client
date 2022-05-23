2912bf8bdc28de6ef9787469ed6886b7903c261a
v4lentin1879 <valentin@berninabahn.com>
2022-05-23 19:36:35 +0200

    changed to https

--- src/api/getDomain.ts
+++ src/api/getDomain.ts
@@ -2,7 +2,7 @@ import {isProduction} from "./isProduction";
 
 export function getDomain(): string {
     const prodUrl = "http://localhost:8080";
-    const devUrl = "http://sopra-fs22-group17-server.herokuapp.com/";
+    const devUrl = "https://sopra-fs22-group17-server.herokuapp.com/";
 
     return isProduction() ? prodUrl : devUrl;
 }
