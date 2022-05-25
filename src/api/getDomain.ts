4e1d4adb7acc20e71ee905b7e5c19929255bfd5d
marcoleder.crypto <marco.leder@uzh.ch>
2022-05-25 15:20:52 +0200

    prodURL

--- src/api/getDomain.ts
+++ src/api/getDomain.ts
@@ -1,11 +1,11 @@
 import {isProduction} from "./isProduction";
 
 export function getDomain(): string {
-    const prodUrl = "http://localhost:8080";
-    const devUrl = "http://localhost:8080";
+    //const prodUrl = "http://localhost:8080";
+    //const devUrl = "http://localhost:8080";
 
-    //const prodUrl = "https://sopra-fs22-group17-server.herokuapp.com/";
-    //const devUrl = "https://sopra-fs22-group17-server.herokuapp.com/";
+    const prodUrl = "https://sopra-fs22-group17-server.herokuapp.com/";
+    const devUrl = "https://sopra-fs22-group17-server.herokuapp.com/";
 
     return isProduction() ? prodUrl : devUrl;
 }
