b5972ea34b732d54f8085e322c4c7bd9a6501dbe
marcoleder.crypto <marco.leder@uzh.ch>
2022-05-29 16:16:32 +0200

    sonarcloud codesmells cleanup

--- src/api/getDomain.ts
+++ src/api/getDomain.ts
@@ -1,11 +1,8 @@
 import { isProduction } from "./isProduction";
 
 export function getDomain(): string {
-    const prodUrl = "http://localhost:8080";
+    const prodUrl = "https://sopra-fs22-group17-server.herokuapp.com/";
     const devUrl = "http://localhost:8080";
-
-    //const prodUrl = "https://sopra-fs22-group17-server.herokuapp.com/";
-    //const devUrl = "https://sopra-fs22-group17-server.herokuapp.com/";
 
     return isProduction() ? prodUrl : devUrl;
 }
