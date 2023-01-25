e216dcb80c00072f3aa55a6271be704f434867ce
Marco Leder <marco.leder@uzh.ch>
2023-01-25 17:47:05 +0100

    revamp domain

--- src/api/getDomain.ts
+++ src/api/getDomain.ts
@@ -1,8 +1,8 @@
 import { isProduction } from "./isProduction";
 
 export function getDomain(): string {
-    const devUrl = "https://ravewavewip.app.runonflux.io/";
-    const prodUrl = "https://ravewavewip.app.runonflux.io/";
+    const devUrl = "https://sopra-fs22-group17-server.herokuapp.com/";
+    const prodUrl = "http://localhost:8080";
 
     return isProduction() ? prodUrl : devUrl;
 }
